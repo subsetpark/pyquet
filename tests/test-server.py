a290950e72b62e04340bd4a5f93a713ba9bec70f
Z. D. Smith <cruxdestruct@gmail.com>
2014-10-04 18:51:28 -0400

    Skeleton test for server

---
+++ tests/test-server.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+from core.game import Partie
+from core.server import Server
+from core.players import Rabelais
+
+class Robot(Server):
+    def __init__(self):
+        player1 = Rabelais('Barry Lyndon')
+        player2 = Rabelais('Rabelais')
+        self.players = {player1, player2}
+        self.partie = Partie(player1, player2)
+
+class TestServer(TestCase):
+    def test_play_robots(self):
+        r = Robot()
+        r.play_a_game()
